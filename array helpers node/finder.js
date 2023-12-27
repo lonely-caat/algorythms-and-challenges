f16216c3b2a86955cd04a6c6299ce80c61d97960
max <33058683+lonely-caat@users.noreply.github.com>
2023-12-27 13:50:48 +0200

    Update finder.js

--- array helpers node/finder.js
+++ array helpers node/finder.js
@@ -3,32 +3,16 @@
 // The object { ladders: '20 feet' } should be used as the search criteria - we would want to find a ladder whose 'height' property was '20 feet';
 //
 // Your goal: Write a 'findWhere' function that achieves this shorthand approach.  'findWhere' should return the found object.
-//
-//     In summary:
-//
+
+
 var ladders = [
         { id: 1, height: 20 },
         { id: 3, height: 25 }
     ];
-//
-// findWhere(ladders, { height: 25 }); // result: { id:3, height: 25 }
-//
-// Hint: the hard part of this is figuring out the name of the proeprty on the criteria.  You can use Object.keys(criteria)[0]
-// to figure out the name of the property on the object.
-// For example, Object.keys({ height: '20 feet' }) would return 'height'.
-// You could then check to see if a given element in the array had a property equal
-// to the criteria's value with something like element[property] === criteria[property].
-//
-//
-//
-
-
-// console.log(ladders.find(l=>l.height===25))
 
 function findWhere(array, criteria){
     let crit = Object.keys(criteria)[0]
     return array.find(l=>l[crit]===criteria[crit])
 
 }
-
 console.log(findWhere(ladders, { height: 25 })); // result: { id:3, height: 25 }
